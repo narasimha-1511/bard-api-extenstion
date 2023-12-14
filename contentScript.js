f068b3c9149b2d2862df9f539e61fdb24c22fe01
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-14 13:06:02 +0530

    adding conetnet js

---
+++ contentScript.js
@@ -0,0 +1,9 @@
+console.log('contentScript.js loaded ')
+;
+console.log("dont worry bard api is here to cheat happily");
+
+const prompt = "Write a story about Narasimha.";
+
+chrome.runtime.sendMessage({message: "textToTextPrompt", prompt:prompt}, function(response) {
+    console.log(response);
+});

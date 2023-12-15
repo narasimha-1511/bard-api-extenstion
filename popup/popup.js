c4071fd12cd2ef18ce57f86b6316baf8c8b19c7a
narasimha-1511 <s.narasimha.2005@gmail.com>
2023-12-15 15:21:28 +0530

    completly updated done

--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,66 @@
-// Add event listener to the send button
-document.getElementById("send-button").addEventListener("click", function () {
-  var userInput = document.getElementById("user-input");
-  var chatbox = document.getElementById("chatbox");
+var sendBtn = document.getElementById("send-button"); // this is send buttom
 
-  // Add user's message to the chatbox
+// Add event listener to the send button
+sendBtn.addEventListener("click", clickFunc);
+
+function clickFunc() {
+  var userInput = document.getElementById("user-input");
+  // Add user's message to the
   var userMessage = document.createElement("p");
   userMessage.classList.add("user-message");
   userMessage.textContent = userInput.value;
   chatbox.appendChild(userMessage);
 
-  // TODO: Add code to send the user's message to the chatbot and get the response
-  // For demo purposes, we'll just add a bot message immediately
-  var botMessage = document.createElement("p");
-  const prompt = userInput.value;
-  console.log(prompt);
-  chrome.runtime.sendMessage(
-    { message: "textToTextPrompt", prompt: prompt },
-    function (response) {
-      botMessage.classList.add("bot-message");
-      botMessage.textContent = response;
-    }
-  );
-  //   botMessage.classList.add("bot-message");
-  //   botMessage.textContent = "I'm a bot, beep boop!";
-  chatbox.appendChild(botMessage);
+  //get the vesion of the bot neeeded to be used
+  var version = document.getElementById("default").checked;
+
+  if (version) {
+    var botMessage = defaultChat();
+  } else {
+    var botMessage = TeluguChat();
+  }
 
   // Clear the user input field
   userInput.value = "";
 
   // Scroll to the bottom of the chatbox
   chatbox.scrollTop = chatbox.scrollHeight;
-});
+}
+
+function defaultChat() {
+  var userInput = document.getElementById("user-input");
+  var chatbox = document.getElementById("chatbox");
+  var botMessage = document.createElement("p");
+
+  const prompt = userInput.value;
+  console.log(prompt);
+  chrome.runtime.sendMessage(
+    { message: "textToTextPrompt", prompt: prompt },
+    function (response) {
+      botMessage.classList.add("bot-message");
+      var pre = document.createElement("pre");
+      pre.textContent = response;
+      botMessage.appendChild(pre);
+      chatbox.appendChild(botMessage);
+    }
+  );
+}
+
+function TeluguChat() {
+  var userInput = document.getElementById("user-input");
+  var chatbox = document.getElementById("chatbox");
+  var botMessage = document.createElement("p");
+
+  const prompt = userInput.value;
+  console.log(prompt);
+  chrome.runtime.sendMessage(
+    { message: "teluguTextToTextPrompt", prompt: prompt },
+    function (response) {
+      botMessage.classList.add("bot-message");
+      var pre = document.createElement("pre");
+      pre.textContent = response;
+      botMessage.appendChild(pre);
+      chatbox.appendChild(botMessage);
+    }
+  );
+}
